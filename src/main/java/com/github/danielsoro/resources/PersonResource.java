ba7437f8ed6b0e737084e884b5355d4ca64123d1
Otavio Santana <otaviopolianasantana@gmail.com>
2019-04-26 11:02:24 -0300

    reduces code with configuration

--- src/main/java/com/github/danielsoro/resources/PersonResource.java
+++ src/main/java/com/github/danielsoro/resources/PersonResource.java
@@ -2,16 +2,22 @@ package com.github.danielsoro.resources;
 
 import com.github.danielsoro.models.Person;
 import com.github.danielsoro.services.PersonRepository;
+import org.jnosql.artemis.ConfigurationUnit;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 import java.util.Optional;
 
 @Path("/person")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class PersonResource {
 
     @Inject
+    @ConfigurationUnit(database = "people")
     private PersonRepository personRepository;
 
     @GET
@@ -20,6 +26,7 @@ public class PersonResource {
         final Optional<Person> person = personRepository.findById(id);
         return person.orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
     }
+
 
     @POST
     public Person createPerson(Person person) {
